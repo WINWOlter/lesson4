d63edeb9bb3b59ca608d0fa2693b3ae8128ff889
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-02-26 09:28:11 +0000

    вчитель може ставити оцінки

--- main.py
+++ main.py
@@ -15,10 +15,40 @@ class Teacher(Person):
   salary = 0
   student = []
 
+  def study(self):
+    self.salary += 1000
+  
+  def __init__(self):
+    super().__init__("Teacher")
+    
+  def set_grade(self,student):
+     student.grade = 12
+
+teacher = Teacher()
+print(teacher.salary)
+
 student = Student()
+
+print("оцінка до",student.grade)
+teacher.set_grade(student)
+print("оцінка після",student.grade)
+    
+
+
 
 print(student.grade)
 
+student.study()
+student.study()
+student.study()
+student.study()
+student.study()
+student.study()
+student.study()
+student.study()
+student.study()
+student.study()
+student.study()
 student.study()
 
 

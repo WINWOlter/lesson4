a8b58e7ed001e69f62c0fac1e2d1f6f79f9c4057
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-02-26 08:54:52 +0000

    зробили так щоб студент вчився

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ class Person:
 
 class Student(Person):
   grade = 0
+  def study(self):
+    self.grade += 1
+
+  def __init__(self):
+    super().__init__("Student")
 
 class Teacher(Person):
   salary = 0
+  student = []
+
+student = Student()
+
+print(student.grade)
+
+student.study()
+
+
+print(student.grade)
+
+
   
